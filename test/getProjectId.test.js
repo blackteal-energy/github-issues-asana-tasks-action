2d9e65b1637c6145bbcd87770cd3c90d90ca73cd
Mustafa Abdullah Kus <mabdkus@hotmail.com>
2025-08-28 12:44:38 +0300

    test: getprojectid.test.js: add new parse api test routines

--- test/getProjectId.test.js
+++ test/getProjectId.test.js
@@ -43,3 +43,47 @@ test("works on large blobs too?", () => {
   const actual = getProjectId(blob);
   expect(actual).toBe(projectId.toString());
 });
+
+test("Get Project ID from new format URL", () => {
+  const projectId = "1211168999004702";
+  const newFormatLink = "https://app.asana.com/1/1202191738631953/project/1211168999004702/list/1211170139684016";
+  const actual = getProjectId(newFormatLink);
+  expect(actual).toBe(projectId);
+});
+
+test("Get Project ID from new format URL with different workspace", () => {
+  const projectId = "1234567890123456";
+  const newFormatLink = "https://app.asana.com/1/9876543210987654/project/1234567890123456/list/1111111111111111";
+  const actual = getProjectId(newFormatLink);
+  expect(actual).toBe(projectId);
+});
+
+test("handles new format URL objects correctly", () => {
+  const projectId = "1211168999004702";
+  const newFormatLink = "https://app.asana.com/1/1202191738631953/project/1211168999004702/list/1211170139684016";
+  const newFormatURL = new URL(newFormatLink);
+  const actual = getProjectId(newFormatURL);
+  expect(actual).toBe(projectId);
+});
+
+test("works with new format in large blobs", () => {
+  const projectId = "1211168999004702";
+  const newFormatLink = "https://app.asana.com/1/1202191738631953/project/1211168999004702/list/1211170139684016";
+  const blob = `Lorem ipsum dolor sit amet ${newFormatLink} consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`;
+
+  const actual = getProjectId(blob);
+  expect(actual).toBe(projectId);
+});
+
+test("backward compatibility - still works with legacy format", () => {
+  const projectId = 1200000987654321;
+  const legacyLink = `https://app.asana.com/0/${projectId}/${projectId}`;
+  const actual = getProjectId(legacyLink);
+  expect(actual).toBe(projectId.toString());
+});
+
+test("returns false for invalid new format URLs", () => {
+  const invalidLink = "https://app.asana.com/1/1202191738631953/task/1211168999004702";
+  const actual = getProjectId(invalidLink);
+  expect(actual).toBe(false);
+});
